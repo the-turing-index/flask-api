918e6c17f941130c4707d2c50a3c415042c19f6c
Michael Evans <michaeljevans113@gmail.com>
2020-11-02 13:26:05 -0700

    Set up calendar structure
    Co-Authored-By: AJ Tran <31839316+ajtran303@users.noreply.github.com>
    Co-Authored-By: ashkanthegreat <airashkan@yahoo.com>

---
+++ data/calendar_list.py
@@ -0,0 +1,25 @@
+from x import y
+from x import y
+from x import y
+from x import y
+from x import y
+from x import y
+from x import y
+from x import y
+
+calendar_list = {
+    "mod1": {
+        "FE": fe1,
+        "BE": be1
+    },
+    "mod2": {
+        "FE": fe2,
+        "BE": be2
+    },
+    "mod3": {
+        "FE": fe3,
+        "BE": be3
+    },
+    "mod4": combined,
+    "community": community
+}
