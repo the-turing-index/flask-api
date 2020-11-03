a337ea15f946038a120e199de5617b4a7a340619
Michael Evans <michaeljevans113@gmail.com>
2020-11-02 20:21:47 -0700

    Import environment variables
    Co-Authored-By: ashkanthegreat <airashkan@yahoo.com>
    Co-Authored-By: AJ Tran <31839316+ajtran303@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from data.calendar_list import calendar_list
+from dotenv import load_dotenv
+
+load_dotenv('config/.env')
+from db.calendars import all_events
 
 app = Flask(__name__)
+app.config.from_pyfile('config/settings.py')
 CORS(app)
 
 @app.route('/api/v1/calendars')
 def index():
-    return jsonify(calendar_list)
+    return jsonify(all_events)
