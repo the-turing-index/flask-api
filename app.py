1d1af2c84c2309dca7294c202605eac054e86e69
AJ Tran <ajtrandev@gmail.com>
2020-11-03 23:06:37 -0700

    Add feature: database service now fetches event data from Google API
      Major Changes
    
      - We have a new script! `python -m db.service`
        - When invoked, it will fetch up-to-date data from Google Calendars
        - A Heroku Scheduler will run it 3 times a day:
          - 12:00AM
          - 8:30AM
          - 12:30PM
    
      Minor Changes
    
      - Removed python-dotenv, as it was not being used
      - Deleted `data/calendars.py` as it was obsolete
    
    Co-authored-by: Michael Evans <62968371+michaeljevans@users.noreply.github.com>
    Co-authored-by: Ashkan Abbasi <47463190+Ashkanthegreat@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-# from dotenv import load_dotenv
-#
-# load_dotenv('config/.env')
 from db.calendars import all_events
 
 app = Flask(__name__)
